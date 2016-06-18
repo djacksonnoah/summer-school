1db7e265b0ef97884743286eb195eed1d78746e2
djacksonnoah <djacksonnoah@gmail.com>
2016-06-17 20:26:28 -0400

    initial setup

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -10,6 +10,49 @@ namespace SummerSchool
     {
         static void Main(string[] args)
         {
+            
+
+            Console.WriteLine( "Chose a number 1) Enroll a student  2) Unenroll a student 3) Print out the list of enrolled students 4) Exit");
+
+            int chooseNumber =  Convert.ToInt32(Console.ReadLine());
+
+            if (chooseNumber == 1)
+
+            {
+                Console.WriteLine("What is the student's first and last name that you want to enroll?");
+
+
+                    }
+
+            else if (chooseNumber == 2)
+
+            {
+                Console.WriteLine("What is the student's first and last name that you want to unenroll?");
+            }
+
+            else if (chooseNumber == 3)
+
+            {
+
+                Console.WriteLine("The list of students is:");
+
+            }
+
+            else if (chooseNumber == 4)
+
+            {
+                Console.WriteLine("Have a nice day");
+
+            }
+
+            else
+
+            {
+                Console.WriteLine("Please choose a number between 1-4!");
+
+            }
+
+            Console.ReadKey();
         }
     }
 }
